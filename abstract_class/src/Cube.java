e4d07abf7933eb0fe74f819aa4fbf6ee7f6fa7c7
ATyuvalev <rouac@mail.ru>
2022-02-22 16:25:42 +0300

    first homework

---
+++ abstract_class/src/Cube.java
@@ -0,0 +1,12 @@
+public class Cube extends Figure{
+
+    @Override
+    public void area() {
+        System.out.println ("Площадь куба");
+    }
+
+    @Override
+    public void volume() {
+        System.out.println ("Объем куба");
+    }
+}
